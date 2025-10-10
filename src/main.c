1c91a7c2fcdbacceead91a71b9a15cafe5019e10
rodrigo.dornelles <rodrigo.dornelles.poa@gmail.com>
2025-10-10 00:30:13 -0300

    feat: trying sinc video/audio/application

--- src/main.c
+++ src/main.c
@@ -8,64 +8,61 @@
 #include <libavutil/imgutils.h>
 #include <libswscale/swscale.h>
 #include <libswresample/swresample.h>
-#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-// --- Constants and Definitions ---
-#define MAX_QUEUE_SIZE 240
-#define VIDEO_FILENAME "/home/rodrigao/Videos/reddresswoman.mp4"
+#define MAX_QUEUE_SIZE 30
+#define VIDEO_FILENAME "/home/rodrigao/Videos/rick-h264.mp4"
 
-// --- Thread-Safe Queue for AVFrame* ---
 typedef struct FrameQueue {
     AVFrame* frames[MAX_QUEUE_SIZE];
     int size;
     int head;
     int tail;
-    pthread_mutex_t mutex;
-    pthread_cond_t cond;
+    uv_mutex_t mutex;
+    uv_cond_t cond;
 } FrameQueue;
 
 void queue_init(FrameQueue* q) {
     memset(q, 0, sizeof(FrameQueue));
-    pthread_mutex_init(&q->mutex, NULL);
-    pthread_cond_init(&q->cond, NULL);
+    uv_mutex_init(&q->mutex);
+    uv_cond_init(&q->cond);
 }
 
 void queue_push(FrameQueue* q, AVFrame* frame) {
-    pthread_mutex_lock(&q->mutex);
+    uv_mutex_lock(&q->mutex);
     while (q->size >= MAX_QUEUE_SIZE) {
-        pthread_cond_wait(&q->cond, &q->mutex);
+        uv_cond_wait(&q->cond, &q->mutex);
     }
     q->frames[q->head] = frame;
     q->head = (q->head + 1) % MAX_QUEUE_SIZE;
     q->size++;
-    pthread_cond_signal(&q->cond);
-    pthread_mutex_unlock(&q->mutex);
+    uv_cond_signal(&q->cond);
+    uv_mutex_unlock(&q->mutex);
 }
 
 AVFrame* queue_pop(FrameQueue* q) {
-    pthread_mutex_lock(&q->mutex);
+    uv_mutex_lock(&q->mutex);
     while (q->size <= 0) {
-        pthread_cond_wait(&q->cond, &q->mutex);
+        uv_cond_wait(&q->cond, &q->mutex);
     }
     AVFrame* frame = q->frames[q->tail];
     q->tail = (q->tail + 1) % MAX_QUEUE_SIZE;
     q->size--;
-    pthread_cond_signal(&q->cond);
-    pthread_mutex_unlock(&q->mutex);
+    uv_cond_signal(&q->cond);
+    uv_mutex_unlock(&q->mutex);
     return frame;
 }
 
 AVFrame* queue_peek(FrameQueue* q) {
-    pthread_mutex_lock(&q->mutex);
+    uv_mutex_lock(&q->mutex);
     AVFrame* frame = NULL;
     if (q->size > 0) {
         frame = q->frames[q->tail];
     }
-    pthread_mutex_unlock(&q->mutex);
+    uv_mutex_unlock(&q->mutex);
     return frame;
 }
 
@@ -74,14 +71,11 @@ void queue_destroy(FrameQueue* q) {
         AVFrame* frame = queue_pop(q);
         av_frame_free(&frame);
     }
-    pthread_mutex_destroy(&q->mutex);
-    pthread_cond_destroy(&q->cond);
+    uv_mutex_destroy(&q->mutex);
+    uv_cond_destroy(&q->cond);
 }
 
-
-// --- Player State ---
 typedef struct PlayerState {
-    // FFmpeg
     AVFormatContext* format_ctx;
     AVCodecContext* video_codec_ctx;
     AVCodecContext* audio_codec_ctx;
@@ -91,32 +85,29 @@ typedef struct PlayerState {
     struct SwrContext* swr_ctx;
     AVRational video_time_base;
 
-    // Queues
     FrameQueue video_q;
     FrameQueue audio_q;
 
-    // libuv
     uv_loop_t* loop;
     uv_work_t decode_req;
     uv_async_t eof_async;
+    uv_timer_t audio_timer;
+    uv_timer_t video_timer;
+    uv_timer_t main_timer;
     int eof;
 
-    // Raylib
     Texture2D video_texture;
     AudioStream audio_stream;
     Image video_image;
 
-    // Sync
+    double start_time;
     double audio_clock;
     double last_video_pts;
 
 } PlayerState;
 
-// --- Global State ---
-// Made global to be accessible by the audio callback which has a fixed signature.
 PlayerState* g_ps = NULL;
 
-// --- Audio Clock ---
 void update_audio_clock(double pts) {
     if (g_ps) g_ps->audio_clock = pts;
 }
@@ -125,30 +116,114 @@ double get_audio_clock() {
     return g_ps ? g_ps->audio_clock : 0.0;
 }
 
-// --- Raylib Audio Callback ---
-void audio_callback(void* buffer, unsigned int frames) {
-    if (!g_ps || !g_ps->swr_ctx) return;
+void main_timer_cb(uv_timer_t* handle);
 
-    float* out = (float*)buffer;
-    int frames_decoded = 0;
+void video_timer_cb(uv_timer_t* handle) {
+    PlayerState* ps = (PlayerState*)handle->data;
 
-    while (frames_decoded < frames) {
-        AVFrame* audio_frame = queue_pop(&g_ps->audio_q);
-        if (!audio_frame) return;
+    double video_clock = GetTime() - ps->start_time;
+    AVFrame* video_frame = queue_peek(&ps->video_q);
 
-        uint8_t* resampled_data[1] = { (uint8_t*)out + frames_decoded * g_ps->audio_codec_ctx->ch_layout.nb_channels * sizeof(float) };
-        int samples_per_channel = swr_convert(g_ps->swr_ctx, resampled_data, frames - frames_decoded, (const uint8_t**)audio_frame->data, audio_frame->nb_samples);
+    if (video_frame) {
+        double video_pts = video_frame->pts * av_q2d(ps->video_time_base);
 
-        if (samples_per_channel > 0) {
-            frames_decoded += samples_per_channel;
-            double pts = audio_frame->pts * av_q2d(g_ps->format_ctx->streams[g_ps->audio_stream_idx]->time_base);
-            update_audio_clock(pts);
+        if (video_pts <= video_clock) {
+            video_frame = queue_pop(&ps->video_q);
+            ps->last_video_pts = video_pts;
+
+            uint8_t* dst[4] = { ps->video_image.data, NULL, NULL, NULL };
+            int dst_linesize[4] = { ps->video_codec_ctx->width * 4, 0, 0, 0 };
+            sws_scale(ps->sws_ctx, (uint8_t const* const*)video_frame->data,
+                      video_frame->linesize, 0, ps->video_codec_ctx->height,
+                      dst, dst_linesize);
+            UpdateTexture(ps->video_texture, ps->video_image.data);
+            av_frame_free(&video_frame);
+
+            uv_timer_start(&ps->video_timer, video_timer_cb, 0, 0);
+        } else {
+            double delay = video_pts - video_clock;
+            uv_timer_start(&ps->video_timer, video_timer_cb, (uint64_t)(delay * 1000), 0);
         }
-        av_frame_free(&audio_frame);
+    } else {
+        uv_timer_start(&ps->video_timer, video_timer_cb, 10, 0);
     }
 }
 
-// --- libuv Decoder Task ---
+void main_timer_cb(uv_timer_t* handle) {
+    PlayerState* ps = (PlayerState*)handle->data;
+
+    if (WindowShouldClose() || ps->eof) {
+        uv_stop(ps->loop);
+        return;
+    }
+
+    BeginDrawing();
+    ClearBackground(BLACK);
+    DrawTexture(ps->video_texture, 0, 0, WHITE);
+    DrawText(TextFormat("A/V Diff: %0.3fs", ps->audio_clock - ps->last_video_pts), 10, 10, 20, LIME);
+    DrawFPS(10, 40);
+    EndDrawing();
+}
+
+void audio_timer_cb(uv_timer_t* handle) {
+    PlayerState* ps = (PlayerState*)handle->data;
+    if (!ps->swr_ctx) {
+        uv_timer_start(&ps->audio_timer, audio_timer_cb, 10, 0);
+        return;
+    }
+
+    if (IsAudioStreamProcessed(ps->audio_stream)) {
+        float* buffer = (float*)calloc(ps->audio_codec_ctx->frame_size, ps->audio_codec_ctx->ch_layout.nb_channels * sizeof(float));
+        if (!buffer) {
+            uv_timer_start(&ps->audio_timer, audio_timer_cb, 10, 0);
+            return;
+        }
+
+        int samples_to_fill = ps->audio_codec_ctx->frame_size * ps->audio_codec_ctx->ch_layout.nb_channels;
+        int buffer_offset = 0;
+
+        while (samples_to_fill > 0) {
+            AVFrame* audio_frame = queue_pop(&ps->audio_q);
+            if (!audio_frame) {
+                memset(buffer + buffer_offset, 0, samples_to_fill * sizeof(float));
+                break;
+            }
+
+            uint8_t* resampled_data = NULL;
+            int max_out_samples = audio_frame->nb_samples * 2;
+            av_samples_alloc(&resampled_data, NULL, ps->audio_codec_ctx->ch_layout.nb_channels, max_out_samples, AV_SAMPLE_FMT_FLT, 0);
+
+            int converted_samples = swr_convert(ps->swr_ctx,
+                                                &resampled_data,
+                                                max_out_samples,
+                                                (const uint8_t**)audio_frame->data,
+                                                audio_frame->nb_samples);
+
+            if (converted_samples > 0) {
+                int samples_to_copy = converted_samples * ps->audio_codec_ctx->ch_layout.nb_channels;
+                if (samples_to_copy > samples_to_fill) {
+                    samples_to_copy = samples_to_fill;
+                }
+                memcpy(buffer + buffer_offset, resampled_data, samples_to_copy * sizeof(float));
+                buffer_offset += samples_to_copy;
+                samples_to_fill -= samples_to_copy;
+            }
+
+            av_freep(&resampled_data);
+            double pts = audio_frame->pts * av_q2d(ps->format_ctx->streams[ps->audio_stream_idx]->time_base);
+            update_audio_clock(pts);
+            av_frame_free(&audio_frame);
+        }
+
+        UpdateAudioStream(ps->audio_stream, buffer, ps->audio_codec_ctx->frame_size);
+        free(buffer);
+    }
+    uv_timer_start(&ps->audio_timer, audio_timer_cb, 0, 0);
+}
+
+void audio_timer_cb(uv_timer_t* handle);
+void video_timer_cb(uv_timer_t* handle);
+
 void decode_task(uv_work_t* req) {
     PlayerState* ps = (PlayerState*)req->data;
     AVPacket* packet = av_packet_alloc();
@@ -182,19 +257,16 @@ void decode_task(uv_work_t* req) {
         av_packet_unref(packet);
     }
     av_packet_free(&packet);
-    uv_async_send(&ps->eof_async); // Signal EOF
+    uv_async_send(&ps->eof_async);
 }
 
-void after_decode_task(uv_work_t* req, int status) {
-    // Cleanup if needed
-}
+void after_decode_task(uv_work_t* req, int status) {}
 
 void on_eof(uv_async_t* handle) {
     PlayerState* ps = (PlayerState*)handle->data;
     ps->eof = 1;
 }
 
-// --- Initialization and Cleanup ---
 int init_ffmpeg(PlayerState* ps, const char* filename) {
     ps->format_ctx = NULL;
     if (avformat_open_input(&ps->format_ctx, filename, NULL, NULL) != 0) return -1;
@@ -205,26 +277,22 @@ int init_ffmpeg(PlayerState* ps, const char* filename) {
 
     if (ps->video_stream_idx < 0 || ps->audio_stream_idx < 0) return -1;
 
-    // --- Video Setup ---
     const AVCodec* video_codec = avcodec_find_decoder(ps->format_ctx->streams[ps->video_stream_idx]->codecpar->codec_id);
     ps->video_codec_ctx = avcodec_alloc_context3(video_codec);
     avcodec_parameters_to_context(ps->video_codec_ctx, ps->format_ctx->streams[ps->video_stream_idx]->codecpar);
     avcodec_open2(ps->video_codec_ctx, video_codec, NULL);
     ps->video_time_base = ps->format_ctx->streams[ps->video_stream_idx]->time_base;
 
-    // --- Audio Setup ---
     const AVCodec* audio_codec = avcodec_find_decoder(ps->format_ctx->streams[ps->audio_stream_idx]->codecpar->codec_id);
     ps->audio_codec_ctx = avcodec_alloc_context3(audio_codec);
     avcodec_parameters_to_context(ps->audio_codec_ctx, ps->format_ctx->streams[ps->audio_stream_idx]->codecpar);
     avcodec_open2(ps->audio_codec_ctx, audio_codec, NULL);
 
-    // --- Rescaler and Resampler ---
     ps->sws_ctx = sws_getContext(ps->video_codec_ctx->width, ps->video_codec_ctx->height, ps->video_codec_ctx->pix_fmt,
                                 ps->video_codec_ctx->width, ps->video_codec_ctx->height, AV_PIX_FMT_RGBA,
                                 SWS_BILINEAR, NULL, NULL, NULL);
 
-    AVChannelLayout out_ch_layout = AV_CHANNEL_LAYOUT_STEREO;
-    swr_alloc_set_opts2(&ps->swr_ctx, &out_ch_layout, AV_SAMPLE_FMT_FLT, 44100,
+    swr_alloc_set_opts2(&ps->swr_ctx, &ps->audio_codec_ctx->ch_layout, AV_SAMPLE_FMT_FLT, ps->audio_codec_ctx->sample_rate,
                         &ps->audio_codec_ctx->ch_layout, ps->audio_codec_ctx->sample_fmt, ps->audio_codec_ctx->sample_rate,
                         0, NULL);
     swr_init(ps->swr_ctx);
@@ -233,7 +301,6 @@ int init_ffmpeg(PlayerState* ps, const char* filename) {
 }
 
 void cleanup(PlayerState* ps) {
-    // Wait for decode thread to finish
     uv_run(ps->loop, UV_RUN_ONCE);
 
     queue_destroy(&ps->video_q);
@@ -247,12 +314,10 @@ void cleanup(PlayerState* ps) {
 
     UnloadTexture(ps->video_texture);
     UnloadImage(ps->video_image);
-    DetachAudioStreamProcessor(ps->audio_stream, audio_callback);
     CloseAudioDevice();
     CloseWindow();
 }
 
-// --- Main ---
 int main(void) {
     g_ps = (PlayerState*)calloc(1, sizeof(PlayerState));
     if (!g_ps) return -1;
@@ -265,14 +330,13 @@ int main(void) {
 
     InitWindow(g_ps->video_codec_ctx->width, g_ps->video_codec_ctx->height, "Raylib Video Player");
     InitAudioDevice();
-    SetTargetFPS(120); // High FPS for smooth polling
+    SetTargetFPS(0);
 
     g_ps->video_image = GenImageColor(g_ps->video_codec_ctx->width, g_ps->video_codec_ctx->height, BLACK);
     g_ps->video_texture = LoadTextureFromImage(g_ps->video_image);
 
-    g_ps->audio_stream = LoadAudioStream(44100, 32, 2);
+    g_ps->audio_stream = LoadAudioStream(g_ps->audio_codec_ctx->sample_rate, 32, g_ps->audio_codec_ctx->ch_layout.nb_channels);
     SetAudioStreamVolume(g_ps->audio_stream, 0.5f);
-    AttachAudioStreamProcessor(g_ps->audio_stream, audio_callback);
     PlayAudioStream(g_ps->audio_stream);
 
     queue_init(&g_ps->video_q);
@@ -285,35 +349,21 @@ int main(void) {
     g_ps->eof_async.data = g_ps;
     uv_async_init(g_ps->loop, &g_ps->eof_async, on_eof);
 
-    while (!WindowShouldClose() && !g_ps->eof) {
-        uv_run(g_ps->loop, UV_RUN_NOWAIT);
+    uv_timer_init(g_ps->loop, &g_ps->video_timer);
+    g_ps->video_timer.data = g_ps;
+    uv_timer_start(&g_ps->video_timer, video_timer_cb, 0, 0);
 
-        AVFrame* video_frame = queue_peek(&g_ps->video_q);
-        if (video_frame) {
-            double video_pts = video_frame->pts * av_q2d(g_ps->video_time_base);
-            double audio_clock = get_audio_clock();
+    uv_timer_init(g_ps->loop, &g_ps->audio_timer);
+    g_ps->audio_timer.data = g_ps;
+    uv_timer_start(&g_ps->audio_timer, audio_timer_cb, 0, 0);
 
-            if (video_pts <= audio_clock) {
-                video_frame = queue_pop(&g_ps->video_q);
-                g_ps->last_video_pts = video_pts;
+    uv_timer_init(g_ps->loop, &g_ps->main_timer);
+    g_ps->main_timer.data = g_ps;
+    uv_timer_start(&g_ps->main_timer, main_timer_cb, 0, 16);
 
-                uint8_t* dst[4] = { g_ps->video_image.data, NULL, NULL, NULL };
-                int dst_linesize[4] = { g_ps->video_codec_ctx->width * 4, 0, 0, 0 };
-                sws_scale(g_ps->sws_ctx, (uint8_t const* const*)video_frame->data,
-                          video_frame->linesize, 0, g_ps->video_codec_ctx->height,
-                          dst, dst_linesize);
-                UpdateTexture(g_ps->video_texture, g_ps->video_image.data);
-                av_frame_free(&video_frame);
-            }
-        }
+    g_ps->start_time = GetTime();
 
-        BeginDrawing();
-        ClearBackground(BLACK);
-        DrawTexture(g_ps->video_texture, 0, 0, WHITE);
-        DrawText(TextFormat("A/V Diff: %0.3fs", g_ps->audio_clock - g_ps->last_video_pts), 10, 10, 20, LIME);
-        DrawFPS(10, 40);
-        EndDrawing();
-    }
+    uv_run(g_ps->loop, UV_RUN_DEFAULT);
 
     cleanup(g_ps);
     free(g_ps);
