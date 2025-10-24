a4306a65a877e09ca0370278211113925e43f35a
RodrigoDornelles <rodrigo.dornelles.poa@gmail.com>
2025-10-24 16:07:33 -0300

    feat: start api system in native frontend

---
+++ lib/Frontend_Api/system.c
@@ -0,0 +1,68 @@
+#include <string.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <ctype.h>
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+#include <lua.h>
+#include <lauxlib.h>
+
+static int lua_native_system_get_language(lua_State *L) {
+    const char *raw = NULL;
+    static char lang[8] = "en-US";
+
+#ifdef _WIN32
+    static wchar_t wbuf[LOCALE_NAME_MAX_LENGTH];
+    static char utf8[16];
+    if (GetUserDefaultLocaleName(wbuf, LOCALE_NAME_MAX_LENGTH) > 0) {
+        WideCharToMultiByte(CP_UTF8, 0, wbuf, -1, utf8, sizeof(utf8), NULL, NULL);
+        raw = utf8;
+    }
+#else
+    raw = getenv("LANG");
+    if (!raw)
+        raw = setlocale(LC_ALL, NULL);
+#endif
+
+    if (raw && strlen(raw) >= 4) {
+        lang[0] = tolower(raw[0]);
+        lang[1] = tolower(raw[1]);
+        lang[2] = '-';
+        lang[3] = toupper(raw[3]);
+        lang[4] = toupper(raw[4]);
+        lang[5] = '\0';
+    }
+
+    lua_pushstring(L, lang);
+    return 1;
+}
+
+static int lua_native_system_get_env(lua_State *L) {
+    const char *name = luaL_checkstring(L, 1);
+    const char *val = getenv(name);
+
+#ifdef _WIN32
+    if (!val) {
+        static char buffer[512];
+        DWORD len = GetEnvironmentVariableA(name, buffer, sizeof(buffer));
+        if (len > 0 && len < sizeof(buffer))
+            val = buffer;
+    }
+#endif
+
+    if (val)
+        lua_pushstring(L, val);
+    else
+        lua_pushnil(L);
+
+    return 1;
+}
+
+
+const luaL_Reg frontend_api_system[] = {
+    {"native_system_get_language", lua_native_system_get_language},
+    {"native_system_get_env", lua_native_system_get_env},
+    {NULL, NULL}
+};
--- lib/Frontend_Core/instance.c
+++ lib/Frontend_Core/instance.c
@@ -12,6 +12,7 @@ extern const luaL_Reg frontend_api_draw[];
 extern const luaL_Reg frontend_api_text[];
 extern const luaL_Reg frontend_api_http[];
 extern const luaL_Reg frontend_api_image[];
+extern const luaL_Reg frontend_api_system[];
 
 gecnd_t *gecnd_new(lua_State* L) {
     gecnd_t *gly = NULL;
@@ -34,11 +35,11 @@ gecnd_t *gecnd_new(lua_State* L) {
         for (int i = 0; frontend_api_text[i].name != NULL; i++) {
             lua_register(L, frontend_api_text[i].name, frontend_api_text[i].func);
         }
-        for (int i = 0; frontend_api_http[i].name != NULL; i++) {
-            lua_register(L, frontend_api_http[i].name, frontend_api_http[i].func);
-        }
         for (int i = 0; frontend_api_image[i].name != NULL; i++) {
             lua_register(L, frontend_api_image[i].name, frontend_api_image[i].func);
+        }
+        for (int i = 0; frontend_api_system[i].name != NULL; i++) {
+            lua_register(L, frontend_api_system[i].name, frontend_api_system[i].func);
         }
 
         gly_hook_luaopen_cjson(L);
