57587067c9b44a3e3c32dcf670ce9f9928b67cd5
Daniel Santos <47725160+DanielSant0s@users.noreply.github.com>
2025-11-02 22:14:05 -0300

    feat: improve hook logic

--- lib/Frontend_Core/iterator.c
+++ lib/Frontend_Core/iterator.c
@@ -3,7 +3,14 @@
 
 bool gecnd_next_input(gecnd_t *gly, char **key, bool *pressed) {
     (void) gly;
-    bool result = false;
-    gly_hook_input_keyboard(key, pressed, &result);
-    return result;
+
+    char *key_ = NULL;
+    bool pressed_ = false;
+    gly_hook_input_keyboard(&key_, &pressed_);
+
+    if (!key_) return false;
+    
+    *key = key_;
+    *pressed = pressed_;
+    return true;
 }
