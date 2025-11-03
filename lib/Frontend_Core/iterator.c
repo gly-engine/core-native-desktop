aee651ed1826f278cb812cc6f2a1c24ed1aad83f
Daniel Santos <47725160+DanielSant0s@users.noreply.github.com>
2025-11-02 22:16:33 -0300

    feat: add initial input hook support (#8)

--- lib/Frontend_Core/iterator.c
+++ lib/Frontend_Core/iterator.c
@@ -1,9 +1,16 @@
 #include "gecnd.h"
+#include "gehook.h"
 
-bool gecnd_next_input(gecnd_t *gly, char** key, bool *pressed)
-{
+bool gecnd_next_input(gecnd_t *gly, char **key, bool *pressed) {
     (void) gly;
-    (void) key;
-    (void) pressed;
-    return false;
+
+    char *key_ = NULL;
+    bool pressed_ = false;
+    gly_hook_input_keyboard(&key_, &pressed_);
+
+    if (!key_) return false;
+    
+    *key = key_;
+    *pressed = pressed_;
+    return true;
 }
